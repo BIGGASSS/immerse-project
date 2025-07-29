f8c43692ca2667778804276b2c2cf9aea0cf2dd2
BIGGASSS <timcooksgrandpa@gmail.com>
2025-07-29 14:41:31 +0100

    Refactor Ship class to include orientation and update createShip function to prompt for orientation input

--- in-class-projects/battleship.py
+++ in-class-projects/battleship.py
@@ -1,33 +1,58 @@
-class Ship():
-    def __init__(self, length, x_pos, y_pos):
+shipList = []
+
+class Ship:
+    def __init__(self, length, x_pos, y_pos, orient):
         self.length = length
         self.x_pos = x_pos
         self.y_pos = y_pos
+        self.orient = orient
     def getLength(self):
         return self.length
     def getX(self):
         return self.x_pos
     def getY(self):
         return self.y_pos
+    def getOrient(self):
+        return self.orient
 
-shipList = []
+class Battlefield:
+    def __init__(self, length):
+        self.length = length
+        self.grid = self.initialisegrid()
+    def displaygrid(self):
+        for x in range(self.length):
+            print()
+            print(self.grid[x])
+    def initialisegrid(self):
+        p1bf = []
+        for x in range(self.length):
+            tmp = []
+            for y in range(self.length):
+                tmp.append("?")
+            p1bf.append(tmp)
+        return p1bf
+
 def listShip():
     for i in range(len(shipList)):
         print(f"Ship number {i+1}")
         print(f"Length: {shipList[i].getLength()}")
         print(f"X Coordinate: {shipList[i].getX()}")
-        print(f"y Coordinate: {shipList[i].getY()}")
+        print(f"Y Coordinate: {shipList[i].getY()}")
+        print(f"Orientation: {shipList[i].getOrient()}")
         print()
 
-
-
 def createShip(n):
-    for i in range(n):
-        l = int(input("Insert the length of your ship"))
-        x = int(input("Insert the x coordinate of your ship"))
-        y = int(input("Insert the y coordinate of your ship"))
-        ship = Ship(l, x, y)
+    for _ in range(n):
+        l = int(input("Insert the length of your ship: "))
+        x = int(input("Insert the x coordinate of your ship: "))
+        y = int(input("Insert the y coordinate of your ship: "))
+        orient = input("Choose the orientation of you ship(left/right/up/down): ")
+        ship = Ship(l, x, y, orient)
         shipList.append(ship)
 
-createShip(3)
+length = int(input("Please input the length of the grid"))
+b1 = Battlefield(length)
+b1.displaygrid()
+
+createShip(1)
 listShip()
